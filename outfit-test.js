741301917a539f51a39aa44a83c63b0ff0c0ad8d
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-15 15:47:26 -0700

    Add tests for adding and removing garments

--- outfit-test.js
+++ outfit-test.js
@@ -10,4 +10,21 @@ describe('Outfit', function() {
     var outfit = new Outfit();
     assert.deepEqual(outfit.garments, [])
   });
+
+  it('should be able to accept new garments', function() {
+    var outfit = new Outfit();
+    assert.deepEqual(outfit.garments, []);
+    outfit.addGarment('Shirt');
+    assert.deepEqual(outfit.garments, ['Shirt']);
+  });
+
+  it('should be able to remove garments', function() {
+    var outfit = new Outfit();
+    assert.deepEqual(outfit.garments, []);
+    outfit.addGarment('Shirt');
+    outfit.addGarment('Pants');
+    assert.deepEqual(outfit.garments, ['Shirt', 'Pants']);
+    outfit.addGarment('Pants');
+    assert.deepEqual(outfit.garments, ['Shirt']);
+  });
 }
