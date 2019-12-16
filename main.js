f0e76ad6d3b3035a4a99711d0b5b16764aaf3a8e
Edward Stoner <edwardlstoner@gmail.com>
2019-12-16 12:16:26 -0700

    Current instance of outfit is updated when garment button is pressed

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ function createOutfit() {
   id++;
   outfits.push(new Outfit(id));
 };
+
+var column1 = document.querySelector('.column1');
+column1.addEventListener('click', addGarment);
+
+function addGarment(event) {
+  var buttonClicked = event.target.innerText;
+  var garmentsArray = outfits[outfits.length - 1].garments;
+  if (event.target.classList.contains('hats-btn')) {
+    if (garmentsArray.indexOf(buttonClicked) === -1){
+      garmentsArray.push(buttonClicked);
+    } else {
+      garmentsArray.splice(garmentsArray.indexOf(buttonClicked), 1);
+    };
+  };
+};
